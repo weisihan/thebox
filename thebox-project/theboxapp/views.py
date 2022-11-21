c92805eb0045a65d0d21f9dea5dc06538796efeb
sandy-yushan-zhang <zys000708@gmail.com>
2022-11-20 21:06:14 -0500

    Add loginuser placeholder view to run django

--- thebox-project/theboxapp/views.py
+++ thebox-project/theboxapp/views.py
@@ -3,21 +3,24 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout 
+from django.contrib.auth import login, logout
 
 # Create your views here.
+
 
 def homepage(request):
     return render(request, 'theboxapp/home.html')
 
+
 def signupuser(request):
     if request.method == "GET":
         return render(request, 'theboxapp/signupuser.html', {'form': UserCreationForm()})
-    else: 
+    else:
         if request.POST['password1'] == request.POST['password2']:
             # create a new user
-            try: 
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1']) 
+            try:
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST['password1'])
                 user.save()
                 login(request, user)
                 return redirect('homepagestudent')
@@ -27,11 +30,16 @@ def signupuser(request):
             # tell the user that 2 passwords don't match
             return render(request, 'theboxapp/signupuser.html', {'form': UserCreationForm(), 'error': 'Passwords do not match'})
 
+
 def logoutuser(request):
     if request.method == "POST":
         logout(request)
         return redirect()
-        
+
 
 def homepagestudent(request):
-     return render(request, 'theboxapp/homepagestudent.html')
+    return render(request, 'theboxapp/homepagestudent.html')
+
+
+def loginuser(request):
+    return HttpResponse('placeholder')
