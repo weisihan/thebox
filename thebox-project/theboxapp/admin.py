63596491ab7095aac77a398dc185723b94d7ebdf
silvia.wei@nyu.edu <silvia.wei@nyu.edu>
2022-11-24 07:21:57 +0900

    created thebox model

--- thebox-project/theboxapp/admin.py
+++ thebox-project/theboxapp/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import DonatedMealSwipe
+from .models import TheBox
 
 class DonatedAdmin(admin.ModelAdmin):
     readonly_fields = ('donatedTime',)
 
+class TheBoxAdmin(admin.ModelAdmin):
+    readonly_fields = ('creationTime',)
+
 # Register your models here.
 admin.site.register(DonatedMealSwipe, DonatedAdmin)
+admin.site.register(TheBox, TheBoxAdmin)
--- thebox-project/theboxapp/models.py
+++ thebox-project/theboxapp/models.py
@@ -3,13 +3,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class DonatedMealSwipe(models.Model):
-    donatedBy = models.CharField(max_length = 10) # netid of the student
-    receivedBy = models.TextField(blank=True) # netid of the student
     donatedTime = models.DateTimeField(auto_now_add=True) # will automatically be set to creation time
     receivedTime = models.DateTimeField(null=True, blank=True)
     availability = models.BooleanField(default=True) # availability of the donated meal swipe
-    donateUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    donateUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='donateUser')
+    receiveUser = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='receiveUser')
 
     def __str__(self):
-        str = "Mealswipe donated by " + self.donatedBy
-        return str
+        return "Mealswipe donated by " + self.donateUser.username
+
+class TheBox(models.Model):
+    nutritionFacts = models.TextField(blank=True) # nutrition facts of the box
+    creationTime = models.DateTimeField(auto_now_add=True) # will automatically be set to creation time
+    receivedTime = models.DateTimeField(null=True, blank=True)
+    availability = models.BooleanField(default=True) # availability of the donated meal swipe
+    receiveUser = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.nutritionFacts
