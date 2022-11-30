602c7f5052c15db12ab3b1c6de9c81dee8935cb2
sandy-yushan-zhang <zys000708@gmail.com>
2022-11-30 05:22:38 -0500

    Add staff/student field and mealSwipeNum field for users

--- thebox-project/theboxapp/admin.py
+++ thebox-project/theboxapp/admin.py
@@ -1,13 +1,40 @@
 from django.contrib import admin
 from .models import DonatedMealSwipe
 from .models import TheBox
+from .models import Account
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
 
 class DonatedAdmin(admin.ModelAdmin):
     readonly_fields = ('donatedTime',)
 
+
 class TheBoxAdmin(admin.ModelAdmin):
     readonly_fields = ('creationTime',)
+
 
 # Register your models here.
 admin.site.register(DonatedMealSwipe, DonatedAdmin)
 admin.site.register(TheBox, TheBoxAdmin)
+
+# class AccountInline / CustomizedUserAdmin: just conventions...
+
+
+class AccountInline(admin.StackedInline):
+    model = Account
+    can_delete = False
+    verbose_name_plural = 'Accounts'
+
+
+class CustomizedUserAdmin(UserAdmin):
+    # override inlines field of UserAdmin
+    inlines = (AccountInline,)
+
+
+# Re-register the existing user admin panel
+admin.site.unregister(User)
+admin.site.register(User, CustomizedUserAdmin)
+
+# could also show account on the panel - MAYBE NO NEED
+admin.site.register(Account)
--- thebox-project/theboxapp/models.py
+++ thebox-project/theboxapp/models.py
@@ -2,23 +2,46 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    who = models.CharField(
+        max_length=7,
+        # ('actual value', 'human readable value')
+        choices=[('none', 'N/A'), ('staff', 'staff'), ('student', 'student')],
+        default='none'
+    )
+    mealSwipNum = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username
+
+
 class DonatedMealSwipe(models.Model):
-    donatedTime = models.DateTimeField(auto_now_add=True) # will automatically be set to creation time
+    # will automatically be set to creation time
+    donatedTime = models.DateTimeField(auto_now_add=True)
     receivedTime = models.DateTimeField(null=True, blank=True)
-    availability = models.BooleanField(default=True) # availability of the donated meal swipe
-    donateUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='donateUser')
-    receiveUser = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='receiveUser')
+    # availability of the donated meal swipe
+    availability = models.BooleanField(default=True)
+    donateUser = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='donateUser')
+    receiveUser = models.ForeignKey(
+        User, null=True, on_delete=models.CASCADE, related_name='receiveUser')
 
     def __str__(self):
         return "Mealswipe donated by " + self.donateUser.username
 
-class TheBox(models.Model):
-    nutritionFacts = models.TextField(blank=True) # nutrition facts of the box
-    creationTime = models.DateTimeField(auto_now_add=True) # will automatically be set to creation time
-    receivedTime = models.DateTimeField(null=True, blank=True)
-    availability = models.BooleanField(default=True) # availability of the donated meal swipe
-    receiveUser = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
 
+class TheBox(models.Model):
+    nutritionFacts = models.TextField(blank=True)  # nutrition facts of the box
+    # will automatically be set to creation time
+    creationTime = models.DateTimeField(auto_now_add=True)
+    receivedTime = models.DateTimeField(null=True, blank=True)
+    # availability of the donated meal swipe
+    availability = models.BooleanField(default=True)
+    receiveUser = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nutritionFacts
