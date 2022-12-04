baf3fb565dc4d9f2b10e3249d8c7a77da403429f
silvia.wei@nyu.edu <silvia.wei@nyu.edu>
2022-12-03 22:19:54 -0500

    fix

--- thebox-project/thebox/settings.py
+++ thebox-project/thebox/settings.py
@@ -124,5 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
     'theboxapp.backends.AccountBackend',
 ]
